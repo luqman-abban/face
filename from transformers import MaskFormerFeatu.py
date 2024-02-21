ca21d87aeef8d7231ba1555d95fb88b366c30345
Luqman Abban <146173414+luqman-abban@users.noreply.github.com>
2024-02-21 16:05:57 +0500

    Add files via upload

---
+++ from transformers import MaskFormerFeatu.py
@@ -0,0 +1,22 @@
+from transformers import MaskFormerFeatureExtractor, MaskFormerForInstanceSegmentation
+from PIL import Image
+import requests
+
+# load MaskFormer fine-tuned on COCO panoptic segmentation
+feature_extractor = MaskFormerFeatureExtractor.from_pretrained("facebook/maskformer-swin-base-coco")
+model = MaskFormerForInstanceSegmentation.from_pretrained("facebook/maskformer-swin-base-coco")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+inputs = feature_extractor(images=image, return_tensors="pt")
+
+outputs = model(**inputs)
+# model predicts class_queries_logits of shape `(batch_size, num_queries)`
+# and masks_queries_logits of shape `(batch_size, num_queries, height, width)`
+class_queries_logits = outputs.class_queries_logits
+masks_queries_logits = outputs.masks_queries_logits
+
+# you can pass them to feature_extractor for postprocessing
+result = feature_extractor.post_process_panoptic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+# we refer to the demo notebooks for visualization (see "Resources" section in the MaskFormer docs)
+predicted_panoptic_map = result["segmentation"]
